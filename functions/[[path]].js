6fa3ffb54710e7a2ede569a6206a7481bb33eff6
priyanshu-sharma <prynsh1112@gmail.com>
2026-05-09 15:35:54 -0400

    added pages setting

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,16 +1,18 @@
+const BACKEND =
+  "https://priyanshu-sharma-b2a37588.fastapicloud.dev"
+
 export async function onRequest(context) {
   const url = new URL(context.request.url)
 
-  const backend =
-    "https://priyanshu-sharma-b2a37588.fastapicloud.dev"
-
-  // Keep full path + query params
   const target =
-    backend + url.pathname + url.search
+    BACKEND + url.pathname + url.search
+
+  const modifiedHeaders = new Headers(context.request.headers)
+  modifiedHeaders.set("host", new URL(BACKEND).host)
 
   const response = await fetch(target, {
     method: context.request.method,
-    headers: context.request.headers,
+    headers: modifiedHeaders,
     body:
       context.request.method !== "GET" &&
       context.request.method !== "HEAD"
